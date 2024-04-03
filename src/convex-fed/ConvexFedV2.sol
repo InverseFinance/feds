620c8acbe64b3a31592f7dc02fb28b3701095286
08xmt <markus.08xmt@gmail.com>
2024-04-03 17:40:03 +0200

    Add ConvexFedV2

---
+++ src/convex-fed/ConvexFedV2.sol
@@ -0,0 +1,258 @@
+pragma solidity ^0.8.13;
+
+import "src/interfaces/IERC20.sol";
+import "src/interfaces/curve/IMetaPool.sol";
+import "src/interfaces/convex/IConvexBooster.sol";
+import "src/interfaces/convex/IConvexBaseRewardPool.sol";
+import "src/convex-fed/CurvePoolAdapterV2.sol";
+
+contract ConvexFedV2 is CurvePoolAdapterV2{
+
+    uint public immutable poolId;
+    IConvexBooster public immutable booster;
+    IConvexBaseRewardPool public immutable baseRewardPool;
+    IERC20 public immutable crv;
+    IERC20 public immutable cvx;
+    address public chair; // Fed Chair
+    address public gov;
+    address public pendingGov;
+    address public guardian;
+    uint public dolaSupply;
+    uint public maxLossExpansionBps;
+    uint public maxLossWithdrawBps;
+    uint public maxLossTakeProfitBps;
+    uint public guardianMaxLossBpsLimit;
+
+    event Expansion(uint amount);
+    event Contraction(uint amount);
+
+    constructor(
+            address _crv,
+            address _cvx,
+            address _crvPoolAddr,
+            address _booster, 
+            address _baseRewardPool, 
+            address _chair,
+            address _gov,
+            address _guardian,
+            uint _maxLossExpansionBps,
+            uint _maxLossWithdrawBps,
+            uint _maxLossTakeProfitBps,
+            uint _guardianMaxLossBpsLimit
+            )
+            CurvePoolAdapterV2(_crvPoolAddr)
+    {
+        booster = IConvexBooster(_booster);
+        baseRewardPool = IConvexBaseRewardPool(_baseRewardPool);
+        crv = IERC20(_crv);
+        cvx = IERC20(_cvx);
+        poolId = baseRewardPool.pid();
+        IERC20(_crvPoolAddr).approve(_booster, type(uint256).max);
+        IERC20(_crvPoolAddr).approve(_baseRewardPool, type(uint256).max);
+        maxLossExpansionBps = _maxLossExpansionBps;
+        maxLossWithdrawBps = _maxLossWithdrawBps;
+        maxLossTakeProfitBps = _maxLossTakeProfitBps;
+        guardianMaxLossBpsLimit = _guardianMaxLossBpsLimit;
+        chair = _chair;
+        gov = _gov;
+        guardian = _guardian;
+    }
+
+    modifier onlyRole(address _role) {
+        require(msg.sender == _role, "Unauthorized");
+        _;
+    }
+
+    modifier onlyRoleOrGov(address _role) {
+        require(msg.sender == _role || msg.sender == gov, "Unauthorized");
+        _;
+    }
+
+    /**
+    * @notice Method for gov to change gov address
+    */
+    function setPendingGov(address _newGov) public onlyRole(gov){
+        pendingGov = _newGov;
+    }
+
+    /**
+    * @notice Claim governance role
+    */
+    function claimGov() public onlyRole(pendingGov){
+        gov = pendingGov;
+        pendingGov = address(0);
+    }
+
+    /**
+    * @notice Method for gov to change the chair
+    */
+    function changeChair(address newChair_) public onlyRole(gov){
+        chair = newChair_;
+    }
+    /**
+    * @notice Method for gov to change the guardian
+    */
+    function changeGuardian(address newGuardian_) public onlyRole(gov){
+        guardian = newGuardian_;
+    }
+
+    /**
+    * @notice Method for current to resign
+    */
+    function resign() public onlyRole(chair){
+        chair = address(0);
+    }
+
+    /**
+    * @notice Set the maximum acceptable loss when expanding dola supply. Only callable by gov.
+    * @param newMaxLossExpansionBps The maximum loss allowed by basis points 1 = 0.01%
+    */
+    function setMaxLossExpansionBps(uint newMaxLossExpansionBps) public onlyRole(gov){
+        require(newMaxLossExpansionBps <= 10000, "Max loss > 100%");
+        maxLossExpansionBps = newMaxLossExpansionBps;
+    }
+
+    /**
+    * @notice Set the maximum acceptable loss when withdrawing dola supply. Only callable by gov or guardian.
+    * @param newMaxLossWithdrawBps The maximum loss allowed by basis points 1 = 0.01%
+    */
+    function setMaxLossWithdrawBps(uint newMaxLossWithdrawBps) public onlyRoleOrGov(guardian){
+        require(newMaxLossWithdrawBps <= 10000, "Max loss > 100%");
+        if(msg.sender == guardian){
+            require(newMaxLossWithdrawBps <= guardianMaxLossBpsLimit, "Max loss > guardian limit");
+        }
+        maxLossWithdrawBps = newMaxLossWithdrawBps;
+    }
+
+    /**
+    * @notice Set the maximum acceptable loss when Taking Profit from LP tokens. Only callable by gov.
+    * @param newMaxLossTakeProfitBps The maximum loss allowed by basis points 1 = 0.01%
+    */
+    function setMaxLossTakeProfitBps(uint newMaxLossTakeProfitBps) public onlyRole(gov){
+        require(newMaxLossTakeProfitBps <= 10000, "Max loss >  100%");
+        maxLossTakeProfitBps = newMaxLossTakeProfitBps;   
+    }
+
+    /**
+    * @notice Set limit of the max loss setable by guardian.
+    * @param newGuardianMaxLossBpsLimit The maximum loss allowed by basis points 1 = 0.01%
+    */
+    function setGuardianMaxLossBpsLimit(uint newGuardianMaxLossBpsLimit) public onlyRole(gov){
+        require(newGuardianMaxLossBpsLimit <= 10000, "Max loss > 100%");
+        guardianMaxLossBpsLimit = newGuardianMaxLossBpsLimit;   
+    }
+
+    /**
+    * @notice Deposits amount of dola tokens into yEarn vault
+    * @param amount Amount of dola token to deposit into yEarn vault
+    */
+    function expansion(uint amount) public onlyRole(chair){
+        dolaSupply += amount;
+        dola.mint(address(this), amount);
+        metapoolDeposit(amount, maxLossExpansionBps);
+        require(booster.depositAll(poolId, true), 'Failed deposit');
+        emit Expansion(amount);
+    }
+
+    /**
+    * @notice Withdraws an amount of dola token to be burnt, contracting DOLA dolaSupply
+    * @dev Be careful when setting maxLoss parameter. There will almost always be some slippage when trading.
+    * For example, slippage + trading fees may be incurred when withdrawing from a Curve pool.
+    * On the other hand, setting the maxLoss too high, may cause you to be front run by MEV
+    * sandwhich bots, making sure your entire maxLoss is incurred.
+    * Recommended to always broadcast withdrawl transactions(contraction & takeProfits)
+    * through a frontrun protected RPC like Flashbots RPC.
+    * @param amountDola The amount of dola tokens to withdraw. Note that more tokens may
+    be withdrawn than requested.
+    */
+    function contraction(uint amountDola) public onlyRole(chair){
+        //Calculate how many lp tokens are needed to withdraw the dola
+        uint crvLpNeeded = lpForDola(amountDola);
+        require(crvLpNeeded <= crvLpSupply(), "Not enough crvLP tokens");
+
+        //Withdraw and unwrap curveLP tokens from convex, but don't claim rewards
+        require(baseRewardPool.withdrawAndUnwrap(crvLpNeeded, false), "Convex withdraw failed");
+
+        //Withdraw DOLA from curve pool
+        uint dolaWithdrawn = metapoolWithdraw(amountDola, maxLossWithdrawBps);
+        require(dolaWithdrawn > 0, "Must contract");
+        uint burnAmount = _burnAndPay();
+        emit Contraction(burnAmount);
+    }
+
+    /**
+    * @notice Withdraws every remaining crvLP token. Can take up to maxLossWithdrawBps in loss, compared to dolaSupply.
+    * It will still be necessary to call takeProfit to withdraw any potential rewards.
+    */
+    function contractAll() public onlyRole(chair){
+        baseRewardPool.withdrawAllAndUnwrap(false);
+        uint dolaMinOut = dolaSupply * (10_000 - maxLossWithdrawBps) / 10_000;
+        crvMetapool.remove_liquidity_one_coin(crvLpSupply(), 0, dolaMinOut);
+        uint burnAmount = _burnAndPay();
+        emit Contraction(burnAmount);
+    }
+
+
+    /**
+    * @notice Withdraws the profit generated by convex staking to governance
+    */
+    function takeProfit() public {
+        require(baseRewardPool.getReward());
+        crv.transfer(gov, crv.balanceOf(address(this)));
+        cvx.transfer(gov, cvx.balanceOf(address(this)));
+    }
+
+    function claimOther(address otherReward) public {
+        require(otherReward != address(dola), "Cant claim dola");
+        require(otherReward != address(crvMetapool), "Cant claim crv LP tokens");
+        IERC20(otherReward).transfer(gov, IERC20(otherReward).balanceOf(address(this)));
+    }
+
+    /**
+    * @notice Burns the remaining dola supply. Useful in case of the FED being completely contracted and wanting to pay off remaining bad debts.
+    */
+    function burnDolaSupply(uint amount) public {
+        if(amount > dolaSupply){
+            dola.transferFrom(msg.sender, address(this), dolaSupply);
+        } else {
+            dola.transferFrom(msg.sender, address(this), amount);
+        }
+        _burnAndPay();
+    }
+
+    /**
+    * @notice Burns all dola tokens held by the fed up to the dolaSupply, taking any surplus as profit.
+    */
+    function _burnAndPay() internal returns(uint burnAmount){
+        uint dolaBal = dola.balanceOf(address(this));
+        if(dolaBal > dolaSupply){
+            IERC20(dola).transfer(gov, dolaBal - dolaSupply);
+            burnAmount = dolaSupply;
+            dolaSupply = 0;
+        } else {
+            burnAmount = dolaBal;
+            dolaSupply -= dolaBal;
+        }
+        IERC20(dola).burn(burnAmount);
+    }
+
+    /**
+    * @notice Withdraws all owned curve LP tokens to governance.
+    * @dev Only to be used in emergency situations and with full consent of governance.
+    * @param claimRewards Claim convex staking rewards if true, otherwise withdraw without claiming.
+    */
+    function emergencyWithdraw(bool claimRewards) external onlyRole(gov) {
+        if(claimRewards){
+            takeProfit();
+        }
+        baseRewardPool.withdrawAllAndUnwrap(false);
+        crvMetapool.transfer(gov, crvMetapool.balanceOf(address(this)));
+    }
+    
+    /**
+    * @notice View function for getting crvLP tokens in the contract + convex baseRewardPool
+    */
+    function crvLpSupply() public view returns(uint){
+        return crvMetapool.balanceOf(address(this)) + baseRewardPool.balanceOf(address(this));
+    }
+}
