fff1f11ef14a2b6f5dff1fe26e5c7966e861cd4a
08xmt <markus.08xmt@gmail.com>
2024-11-28 11:21:56 +0100

    Add DromeFarmer

--- src/drome-fed/DromeFarmer.sol
+++ src/drome-fed/DromeFarmer.sol
@@ -18,7 +18,7 @@ contract DromeFarmer is L1SuperchainGovernable{
     uint public maxSlippageBpsStableToDola;
     uint public maxSlippageBpsLiquidity;
 
-    uint public constant bridgedUSDC_MULTIPLIER= 1e12;
+    uint public constant pairedStable_MULTIPLIER= 1e12;
     uint public constant PRECISION = 10_000;
     uint32 public constant MAINNET_CCTP_DOMAIN = 0;
 
@@ -52,9 +52,7 @@ contract DromeFarmer is L1SuperchainGovernable{
         fortKnox = addresses[4];
         cctp = ICCTP(addresses[5]);
 
-        maxSlippageBpsDolaToUsdc = maxSlippageBps[0];
-        maxSlippageBpsUsdcToDola = maxSlippageBps[1];
-        maxSlippageBpsUsdcToUsdc = maxSlippageBps[2];
+        maxSlippageBpsStableToDola = maxSlippageBps[1];
         maxSlippageBpsLiquidity = maxSlippageBpsLiquidity_;
     }
 
@@ -69,9 +67,9 @@ contract DromeFarmer is L1SuperchainGovernable{
 
 
     /**
-     * @notice Attempts to deposit `dolaAmount` of DOLA & `stableAmount` of bridgedUSDC into Aerodrome DOLA/bridgedUSDC stable pool. Then, deposits LP tokens into gauge.
-     * @param dolaAmount Amount of DOLA to be added as liquidity in Aerodrome DOLA/bridgedUSDC pool
-     * @param stableAmount Amount of bridgedUSDC to be added as liquidity in Aerodrome DOLA/bridgedUSDC pool
+     * @notice Attempts to deposit `dolaAmount` of DOLA & `stableAmount` of pairedStable into Aerodrome DOLA/pairedStable stable pool. Then, deposits LP tokens into gauge.
+     * @param dolaAmount Amount of DOLA to be added as liquidity in Aerodrome DOLA/pairedStable pool
+     * @param stableAmount Amount of pairedStable to be added as liquidity in Aerodrome DOLA/pairedStable pool
      */
     function _deposit(uint dolaAmount, uint stableAmount) internal {
         uint lpTokenPrice = getLpTokenPrice();
@@ -81,7 +79,7 @@ contract DromeFarmer is L1SuperchainGovernable{
         (uint dolaSpent, uint stableSpent, uint lpTokensReceived) = router.addLiquidity(address(DOLA), address(pairedStable), true, dolaAmount, stableAmount, 0, 0, address(this), block.timestamp);
         require(lpTokensReceived > 0, "No LP tokens received");
 
-        uint totalDolaValue = stableSpent * bridgedUSDC_MULTIPLIER + dolaSpent;
+        uint totalDolaValue = stableSpent * pairedStable_MULTIPLIER + dolaSpent;
 
         uint expectedLpTokens = applySlippage(totalDolaValue * 1e18 / lpTokenPrice, maxSlippageBpsLiquidity);
         if (lpTokensReceived < expectedLpTokens) revert LiquiditySlippageTooHigh();
@@ -92,26 +90,26 @@ contract DromeFarmer is L1SuperchainGovernable{
     }
 
     /**
-     * @notice Attempts to deposit `dolaAmount` of DOLA & `stableAmount` of bridgedUSDC into Aerodrome DOLA/bridgedUSDC stable pool. Then, deposits LP tokens into gauge.
-     * @param dolaAmount Amount of DOLA to be added as liquidity in Aerodrome DOLA/bridgedUSDC pool
-     * @param stableAmount Amount of bridgedUSDC to be added as liquidity in Aerodrome DOLA/bridgedUSDC pool
+     * @notice Attempts to deposit `dolaAmount` of DOLA & `stableAmount` of pairedStable into Aerodrome DOLA/pairedStable stable pool. Then, deposits LP tokens into gauge.
+     * @param dolaAmount Amount of DOLA to be added as liquidity in Aerodrome DOLA/pairedStable pool
+     * @param stableAmount Amount of pairedStable to be added as liquidity in Aerodrome DOLA/pairedStable pool
      */
     function deposit(uint dolaAmount, uint stableAmount) external onlyRoleOrGovernor(l2Chair) {
         _deposit(dolaAmount, stableAmount);
     }
 
     /**
-     * @notice Calls `deposit()` with entire DOLA & bridgedUSDC token balance of this contract.
+     * @notice Calls `deposit()` with entire DOLA & pairedStable token balance of this contract.
      */
     function depositAll() external onlyRoleOrGovernor(l2Chair) {
         _deposit(DOLA.balanceOf(address(this)), pairedStable.balanceOf(address(this)));
     }
 
     /**
-     * @notice Withdraws `dolaAmount` worth of LP tokens from gauge. Then, redeems LP tokens for DOLA/bridgedUSDC.
+     * @notice Withdraws `dolaAmount` worth of LP tokens from gauge. Then, redeems LP tokens for DOLA/pairedStable.
      * @dev If attempting to remove more DOLA than total LP tokens are worth, will remove all LP tokens.
-     * @param dolaAmount Desired dola value to remove from DOLA/bridgedUSDC pool. Will attempt to remove 50/50 while allowing for `maxSlippageBpsLiquidity` bps of variance.
-     * @return Amount of bridgedUSDC received from liquidity removal. Used by withdrawLiquidityAndSwap wrapper.
+     * @param dolaAmount Desired dola value to remove from DOLA/pairedStable pool. Will attempt to remove 50/50 while allowing for `maxSlippageBpsLiquidity` bps of variance.
+     * @return Amount of pairedStable received from liquidity removal. Used by withdrawLiquidityAndSwap wrapper.
      */
     function _withdrawLiquidity(uint dolaAmount) internal returns (uint) {
         uint lpTokenPrice = getLpTokenPrice();
@@ -122,50 +120,41 @@ contract DromeFarmer is L1SuperchainGovernable{
         dolaGauge.withdraw(liquidityToWithdraw);
    
         LP_TOKEN.approve(address(router), liquidityToWithdraw);
-        (uint amountbridgedUSDC, uint amountDola) = router.removeLiquidity(address(pairedStable), address(DOLA), true, liquidityToWithdraw, 0, 0, address(this), block.timestamp);
+        (uint amountpairedStable, uint amountDola) = router.removeLiquidity(address(pairedStable), address(DOLA), true, liquidityToWithdraw, 0, 0, address(this), block.timestamp);
 
-        uint totalDolaReceived = amountDola + (amountbridgedUSDC *bridgedUSDC_MULTIPLIER);
+        uint totalDolaReceived = amountDola + (amountpairedStable *pairedStable_MULTIPLIER);
 
         if (applySlippage(dolaAmount, maxSlippageBpsLiquidity) > totalDolaReceived) {
             revert LiquiditySlippageTooHigh();
         }
 
-        return amountbridgedUSDC;
+        return amountpairedStable;
     }
 
     /**
-     * @notice Withdraws `dolaAmount` worth of LP tokens from gauge. Then, redeems LP tokens for DOLA/bridgedUSDC.
+     * @notice Withdraws `dolaAmount` worth of LP tokens from gauge. Then, redeems LP tokens for DOLA/pairedStable.
      * @dev If attempting to remove more DOLA than total LP tokens are worth, will remove all LP tokens.
-     * @param dolaAmount Desired dola value to remove from DOLA/bridgedUSDC pool. Will attempt to remove 50/50 while allowing for `maxSlippageBpsLiquidity` bps of variance.
-     * @return Amount of bridgedUSDC received from liquidity removal. Used by withdrawLiquidityAndSwap wrapper.
+     * @param dolaAmount Desired dola value to remove from DOLA/pairedStable pool. Will attempt to remove 50/50 while allowing for `maxSlippageBpsLiquidity` bps of variance.
+     * @return Amount of pairedStable received from liquidity removal. Used by withdrawLiquidityAndSwap wrapper.
      */
     function withdrawLiquidity(uint dolaAmount) external onlyRoleOrGovernor(l2Chair) returns (uint) {
         return _withdrawLiquidity(dolaAmount);
     }
  
     /**
-     * @notice Withdraws `dolaAmount` worth of LP tokens from gauge. Then, redeems LP tokens for DOLA/bridgedUSDC and swaps redeemed bridgedUSDC to DOLA.
-     * @param dolaAmount Desired dola value to remove from DOLA/bridgedUSDC pool. Will attempt to remove 50/50 while allowing for `maxSlippageBpsLiquidity` bps of variance.
+     * @notice Withdraws `dolaAmount` worth of LP tokens from gauge. Then, redeems LP tokens for DOLA/pairedStable and swaps redeemed pairedStable to DOLA.
+     * @param dolaAmount Desired dola value to remove from DOLA/pairedStable pool. Will attempt to remove 50/50 while allowing for `maxSlippageBpsLiquidity` bps of variance.
      */
     function withdrawLiquidityAndSwapToDOLA(uint dolaAmount) external onlyRoleOrGovernor(l2Chair) {
         uint stableAmount = _withdrawLiquidity(dolaAmount);
 
         swapStabletoDOLA(Amount);
     }
-    /**
-     * @notice Withdraws `dolaAmount` of DOLA to fortKnox on L1. Will take 7 days before withdraw is claimable on L1.
-     * @param dolaAmount Amount of DOLA to withdraw and send to L1 FortKnox
-     */
-    function withdrawToL1FortKnox(uint dolaAmount) external onlyRoleOrGovernor(l2Chair) {
-        if (dolaAmount > DOLA.balanceOf(address(this))) revert NotEnoughTokens();
-
-        bridge.withdrawTo(address(DOLA), fortKnox, dolaAmount, 0, "");
-    }
 
     /**
-     * @notice Withdraws `dolaAmount` of DOLA & `stableAmount` of bridgedUSDC to fortKnox on L1. Will take 7 days before withdraw is claimable on L1.
+     * @notice Withdraws `dolaAmount` of DOLA & `stableAmount` of pairedStable to fortKnox on L1. Will take 7 days before withdraw is claimable on L1.
      * @param dolaAmount Amount of DOLA to withdraw and send to L1 FortKnox
-     * @param stableAmount Amount of bridgedUSDC to withdraw and send to L1 FortKnox
+     * @param stableAmount Amount of pairedStable to withdraw and send to L1 FortKnox
      */
     function withdrawToFortKnox(uint dolaAmount, uint stableAmount) external onlyRoleOrGovernor(l2Chair) {
         if (dolaAmount > DOLA.balanceOf(address(this))) revert NotEnoughTokens();
@@ -173,27 +162,8 @@ contract DromeFarmer is L1SuperchainGovernable{
 
         DOLA.transfer(dolaAmount, fortKnox);
         pairedStable.transfer(stableAmount. fortKnox);
-        bridge.withdrawTo(address(DOLA), fortKnox, dolaAmount, 0, "");
-        pairedStable.approve(address(cctp), stableAmount);
-        cctp.depositForBurn(stableAmount, MAINNET_CCTP_DOMAIN, bytes32(uint256(uint160(fortKnox))), address(pairedStable));
     }
 
-    function withdrawToL1FortKnoxNative(uint stableAmount) external onlyRoleOrGovernor(l2Chair) {
-        if (stableAmount > pairedStable.balanceOf(address(this))) revert NotEnoughTokens();
-        
-        pairedStable.approve(address(cctp), stableAmount);
-        cctp.depositForBurn(stableAmount, MAINNET_CCTP_DOMAIN, bytes32(uint256(uint160(fortKnox))), address(pairedStable));
-    }
-
-    /**
-     * @notice Withdraws `stableAmount` of bridgedUSDC to fortKnox on L1. Will take 7 days before withdraw is claimable.
-     * @param stableAmount Amount of bridgedUSDC to withdraw and send to L1 FortKnox
-     */
-    function withdrawToL1FortKnoxBridged(uint stableAmount) external onlyRoleOrGovernor(l2Chair) {
-        if (stableAmount > bridgedUSDC.balanceOf(address(this))) revert NotEnoughTokens();
-
-        bridge.withdrawTo(address(bridgedUSDC), fortKnox, stableAmount, 0, "");
-    }
     /**
      * @notice Withdraws `amount` of `l2Token` to address `to` on L1. Will take 7 days before withdraw is claimable.
      * @param l2Token Address of the L2 token to be withdrawn
@@ -201,30 +171,30 @@ contract DromeFarmer is L1SuperchainGovernable{
      */
     function withdrawTokensToL1(address l2Token, uint amount) external onlyRoleOrGovernor(l2Chair) {
         if (amount > IERC20(l2Token).balanceOf(address(this))) revert NotEnoughTokens();
-        if(l2Token == address(DOLA) || l2Token == address(pairedStable) || l2Token == address(bridgedUSDC) ) revert RestrictedToken();
+        if(l2Token == address(DOLA) || l2Token == address(pairedStable) || l2Token == address(pairedStable) ) revert RestrictedToken();
 
         IERC20(l2Token).approve(address(bridge), amount);
         bridge.withdrawTo(l2Token, l1Treasury, amount, 0, "");
     }
 
     /**
-     * @notice Swap `stableAmount` of bridgedUSDC to DOLA through aerodrome.
-     * @param stableAmount Amount of bridgedUSDC to swap to DOLA
+     * @notice Swap `stableAmount` of pairedStable to DOLA through aerodrome.
+     * @param stableAmount Amount of pairedStable to swap to DOLA
      */
     function swapStableToDOLA(uint stableAmount) external onlyRoleOrGovernor(l2Chair) {
-        uint minOut = applySlippage(stableAmount, maxSlippageBpsUsdcToDola) * bridgedUSDC_MULTIPLIER;
+        uint minOut = applySlippage(stableAmount, maxSlippageBpsStableToDola) * pairedStable_MULTIPLIER;
 
-        bridgedUSDC.approve(address(router), stableAmount);
-        router.swapExactTokensForTokens(stableAmount, minOut, getRoute(address(bridgedUSDC), address(DOLA)), address(this), block.timestamp);
+        pairedStable.approve(address(router), stableAmount);
+        router.swapExactTokensForTokens(stableAmount, minOut, getRoute(address(pairedStable), address(DOLA)), address(this), block.timestamp);
     }
 
 
     /**
-     * @notice Calculates approximate price of 1 Aerodrome DOLA/bridgedUSDC stable pool LP token
+     * @notice Calculates approximate price of 1 Aerodrome DOLA/pairedStable stable pool LP token
      */
     function getLpTokenPrice() internal view returns (uint) {
         (uint dolaAmountOneLP, uint stableAmountOneLP) = router.quoteRemoveLiquidity(address(DOLA), address(pairedStable), true, poolFactory, 0.001 ether);
-        stableAmountOneLP *= bridgedUSDC_MULTIPLIER;
+        stableAmountOneLP *= pairedStable_MULTIPLIER;
         return (dolaAmountOneLP + stableAmountOneLP)*1000;
     }
 
@@ -253,8 +223,8 @@ contract DromeFarmer is L1SuperchainGovernable{
     }
 
     /**
-     * @notice Governance only function for setting acceptable slippage when swapping DOLA -> bridgedUSDC
-     * @param newMaxSlippageBps The new maximum allowed loss for DOLA -> bridgedUSDC swaps. 1 = 0.01%
+     * @notice Governance only function for setting acceptable slippage when swapping DOLA -> pairedStable
+     * @param newMaxSlippageBps The new maximum allowed loss for DOLA -> pairedStable swaps. 1 = 0.01%
      */
     function setMaxSlippageDolaToUsdc(uint newMaxSlippageBps) onlyGovernor external {
         if (newMaxSlippageBps > 10000) revert MaxSlippageTooHigh();
@@ -262,22 +232,8 @@ contract DromeFarmer is L1SuperchainGovernable{
     }
 
     /**
-     * @notice Governance only function for setting acceptable slippage when swapping bridgedUSDC -> DOLA
-     * @param newMaxSlippageBps The new maximum allowed loss for bridgedUSDC -> DOLA swaps. 1 = 0.01%
-     */
-    function setMaxSlippageUsdcToDola(uint newMaxSlippageBps) onlyGovernor external {
-        if (newMaxSlippageBps > 10000) revert MaxSlippageTooHigh();
-        maxSlippageBpsUsdcToDola = newMaxSlippageBps;
-    }
-
-    function setMaxSlippageUsdcToUsdc(uint newMaxSlippageBps) onlyGovernor external {
-        if (newMaxSlippageBps > 10000) revert MaxSlippageTooHigh();
-        maxSlippageBpsUsdcToUsdc = newMaxSlippageBps;
-    }
-
-    /**
-     * @notice Governance only function for setting acceptable slippage when adding or removing liquidty from DOLA/bridgedUSDC pool
-     * @param newMaxSlippageBps The new maximum allowed loss for adding/removing liquidity from DOLA/bridgedUSDC pool. 1 = 0.01%
+     * @notice Governance only function for setting acceptable slippage when adding or removing liquidty from DOLA/pairedStable pool
+     * @param newMaxSlippageBps The new maximum allowed loss for adding/removing liquidity from DOLA/pairedStable pool. 1 = 0.01%
      */
     function setMaxSlippageLiquidity(uint newMaxSlippageBps) onlyGovernor external {
         if (newMaxSlippageBps > 10000) revert MaxSlippageTooHigh();
@@ -302,7 +258,7 @@ contract DromeFarmer is L1SuperchainGovernable{
 
     /**
      * @notice Method for gov to change the L1 fortKnox address
-     * @dev fortKnox is the L1 address that receives all bridged DOLA/bridgedUSDC from both withdrawToL1FortKnox functions
+     * @dev fortKnox is the L1 address that receives all bridged DOLA/pairedStable from both withdrawToL1FortKnox functions
      * @param newFortKnox_ L1 address to be set as fortKnox
      */
     function changeFortKnox(address newFortKnox_) external onlyGovernor {
